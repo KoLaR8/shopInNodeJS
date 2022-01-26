8396de0ddea5f43a0a04a3d123abc4c858870e35
Wojtek Kolarczyk <wkolarczyk99@gmail.com>
2022-01-26 01:43:05 +0100

    reading products from db on mainPage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'shopWWW'
+})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { session: false});
+  connection.query("SELECT name, category_id FROM products", (err, rows) => {
+    if(err) throw err;
+    res.render('index', {data: rows});
+  })
+
 });
 
 module.exports = router;
